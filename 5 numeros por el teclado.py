e281437113b320a067828011eb43c8ccac6ab851
Dervinransiel <78989353+Dervinransiel@users.noreply.github.com>
2021-02-12 21:38:00 -0400

    Add files via upload

---
+++ 5 numeros por el teclado.py
@@ -0,0 +1,11 @@
+# Crear un programa que permita cargar 5 números por teclado y los muestre.
+
+numero_uno = input("Ingrese un numero:")
+numero_dos = input("Ingrese otro numero:")
+numero_tres = input("Ingrese un numero:")
+numero_cuatro = input("Ingrese un numero:")
+numero_cinco = input("ingrese ultimo numero:")
+
+sumar = numero_uno + numero_dos + numero_tres
+
+print(f"Numeros Digitados{numero_uno,numero_dos,numero_tres,numero_cuatro,numero_cinco}")
