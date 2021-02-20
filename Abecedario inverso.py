4ad187d8979d55b0965816e48607e163678eff75
Dervinransiel <78989353+Dervinransiel@users.noreply.github.com>
2021-02-19 20:34:36 -0400

    Add files via upload

---
+++ Abecedario inverso.py
@@ -0,0 +1,12 @@
+# Crear un programa que muestre las letras de la 
+# Z (mayúscula) a la A (mayúscula, descendiendo).
+
+palabra = []
+
+palabra = input("ingrese la palabra a invertir:\n")
+palabrare = ""
+
+for i in palabra:
+    palabrare = i + palabrare
+
+    print(palabrare)
