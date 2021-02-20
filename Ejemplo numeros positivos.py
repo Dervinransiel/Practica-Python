6082fc2a7ecf1f4bfb5289d8f1d6f6cc332b4c94
Dervinransiel <78989353+Dervinransiel@users.noreply.github.com>
2021-02-19 20:29:10 -0400

    Add files via upload

---
+++ Ejemplo numeros positivos.py
@@ -0,0 +1,21 @@
+
+comprobar = True
+
+while comprobar == True:
+
+    numero_1 = int(input("ingresar un numero:"))
+    numero_2 = int(input("ingresar otro numero:"))
+
+    sumar = (numero_1 + numero_2)
+    
+    print("la suma es:" + str(sumar))
+
+    if n > 0:
+
+        comprobar = True
+
+        print("El numero es positivos"+ str(sumar)) 
+
+        comprobar = False
+
+        print("El numero ingresado es negativo no es correcto",negativo)
